b7d694de1c195668e74125a204cd81dd814907d9
Alexander Saharchuk <pioneer@saharchuk.com>
2013-01-30 23:58:57 +0200

    Import newrelic version 0.4.2

---
+++ newrelic/metadata.rb
@@ -0,0 +1,18 @@
+maintainer       "Escape Studios"
+maintainer_email "dev@escapestudios.com"
+license          "MIT"
+description      "Installs/Configures New Relic"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.4.2"
+
+supports "ubuntu"
+supports "debian"
+supports "centos"
+supports "redhat"
+supports "fedora"
+supports "scientific"
+supports "amazon"
+
+recommends "apache2"
+recommends "php"
+recommends "python"
